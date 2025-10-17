10853e0164915cc46c2a3b9936f2b32901fbf510
Ilias Benjelloun <ilias.benjelloun@alerion.fr>
2025-10-17 18:48:14 +0200

    refactor: simplify getSize function in useBreakpoints hook for improved readability
    - Removed unused isSmall and isMedium functions, directly integrating their logic into the getSize function.
    - Enhanced code clarity by streamlining the breakpoint evaluation process.

--- src/hooks/useBreakpoints.ts
+++ src/hooks/useBreakpoints.ts
@@ -2,11 +2,8 @@ import * as React from 'react'
 
 export type Size = 'large' | 'medium' | 'small'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const isSmall = (width: number, height: number) => width < 600
-const isMedium = (width: number, height: number) => width < 1000 || height < 680
 const getSize = (width: number, height: number) =>
-  isSmall(width, height) ? 'small' : isMedium(width, height) ? 'medium' : 'large'
+  width < 600 ? 'small' : width < 1000 || height < 680 ? 'medium' : 'large'
 
 export default function useBreakpoints(breakpoint = { width: 1000, height: 680 }) {
   const [size, setSize] = React.useState<Size>(
